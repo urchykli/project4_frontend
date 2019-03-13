2f90cf0c9636ab0ff0ade8bc6b8006e3b53d2dfa
Lindsay Urchyk <urchykli@gmail.com>
2019-03-12 20:53:26 -0400

    breweries without lat long have been weeded out

--- p4_frontend/src/Home.js
+++ p4_frontend/src/Home.js
@@ -1,87 +1,123 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import Map from './Map'
-
+import Map from "./Map";
+import { Link } from "react-router-dom";
+import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
+import icon from "./location.svg";
 
 class Home extends Component {
-	constructor() {
-		super()
-	
-		this.state = {
-			city: null,
-			state: null,
-			breweries: []
-		}
-	}
-	handleCityInput(e) {
-		this.setState({
-		  city: e.target.value
-		})
-	  }
-	
-	  handleStateInput(e) {
-		this.setState({
-		  state: e.target.value
-		})
-	  }
-	  handleSearchSubmit(e) {
-		e.preventDefault()
-		const url = `https://api.openbrewerydb.org/breweries?by_city=${this.state.city}&by_state=${this.state.state}`
-		console.log(url)
-		axios.get(url)
-		.then(res => {
-			this.setState({
-				breweries: res.data
-			})
-			console.log(this.state.breweries)
-		})
-		.catch((err) => {
-		  console.log(err)
-		})
-	  }
+    constructor() {
+        super();
 
-	// componentDidMount() {
-		// const zip = this.props.match.params.postalCode;
-		// const hardCity = 'Alexandria';
-		// const hardState = 'virginia'
+        this.state = {
+            city: null,
+            state: null,
+            breweries: [],
+            marker: {
+                latitude: null,
+                longitude: null
+            },
+            realBrewery: [],
+            name: false
+        };
+    }
 
-		// const latLong = `https://api.mapbox.com/geocoding/v5/mapbox.places/${zip}.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1552333204825&autocomplete=true&types=postcode`
-		// const url = `${coindeskURL}${currency}.json`
-		// axios.get(latLong)
-		// .then(res => {
-		// })
-		// .then()
-		// .catch(err => {
-		// 	console.error(err)
-		// })
-	// }
-	// componentDidUpdate() {
+    handleCityInput(e) {
+        this.setState({
+            city: e.target.value
+        });
+    }
 
-	// 	// const url = `https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${this.state.coordinates[0]}&lng=${this.state.coordinates[1]}&key=0c157ba02d36883acc99becbb6b30e30`
+    handleStateInput(e) {
+        this.setState({
+            state: e.target.value
+        });
+    }
+    handleSearchSubmit(e) {
+        e.preventDefault();
+        const url = `https://api.openbrewerydb.org/breweries?by_city=${
+            this.state.city
+        }&by_state=${this.state.state}`;
+        console.log(url);
+        axios
+            .get(url)
+            .then(res => {
+                this.setState({
+                    breweries: res.data
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
-	// }
+    }
 
-	render() {
+    // componentDidMount() {
+    // const zip = this.props.match.params.postalCode;
+    // const hardCity = 'Alexandria';
+    // const hardState = 'virginia'
 
-		return (
-			<div className="search">
-				<form className="search-form" onSubmit={(e) => this.handleSearchSubmit(e)}>
-				<h3>Enter a city and state to find breweries near you!</h3>
-				<p>
-				<label>City:</label>
-				<textarea onChange={(e) => this.handleCityInput(e)}></textarea>
-				</p>
-				<p>
-				<label>State: </label>
-				<textarea onChange={(e) => this.handleStateInput(e)}>
-				</textarea>
-				</p>
-				<input type="submit" value="Submit"/>
-			</form>
-			<Map />
-			</div>
-		);
-	}
+    // const latLong = `https://api.mapbox.com/geocoding/v5/mapbox.places/${zip}.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1552333204825&autocomplete=true&types=postcode`
+    // axios.get(latLong)
+    // .then(res => {
+    // })
+    // .then()
+    // .catch(err => {
+    // 	console.error(err)
+    // })
+    // }
+componentDidUpdate() {
+    this.state.realBrewery = []
+    if(!this.state.name) {
+    for (let i = 0; i < this.state.breweries.length; i++) {
+        if (this.state.breweries[i].longitude !== null) {
+                this.state.realBrewery.push(this.state.breweries[i])
+                this.state.name = true
+        }
+        console.log(this.state.realBrewery);
+    }
+    }
+}
+
+    render() {
+        let markers = this.state.realBrewery.map(brewery => {
+            if (brewery.latitude != null) {
+            return (
+                <Marker
+                    latitude={brewery.latitude}
+                    longitude={brewery.longitude}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                >
+                    <Link to={brewery._id}>
+                        <img src={icon} width="15" height="50" />
+                    </Link>
+                </Marker>
+            );
+            }
+        });
+        return (
+            <div className="search">
+                <form
+                    className="search-form"
+                    onSubmit={e => this.handleSearchSubmit(e)}
+                >
+                    <h3>Enter a city and state to find breweries near you!</h3>
+                    <p>
+                        <label>City: </label>
+                        <textarea onChange={e => this.handleCityInput(e)} />
+                    </p>
+                    <p>
+                        <label>State: </label>
+                        <textarea onChange={e => this.handleStateInput(e)} />
+                    </p>
+                    <input type="submit" value="Submit" />
+                </form>
+                <Map {...markers}/>
+                
+            </div>
+        );
+    }
 }
 
 export default Home;
--- p4_frontend/src/Map.js
+++ p4_frontend/src/Map.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 // import "./Map.css";
 import axios from "axios";
-import ReactMapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
-import icon from './location.svg'
+import ReactMapGL, { Marker, Popup, NavigationControl } from "react-map-gl";
+import icon from "./location.svg";
 
-
-const TOKEN = 'pk.eyJ1IjoidXJjaHlrbGkiLCJhIjoiY2p0Mzk3d2c2MGtnNDN5bHNqaDByOTh0cyJ9.w36OVXzFUTpeyqf0vcvjDA'; // Set your mapbox token here
+const TOKEN =
+  "pk.eyJ1IjoidXJjaHlrbGkiLCJhIjoiY2p0Mzk3d2c2MGtnNDN5bHNqaDByOTh0cyJ9.w36OVXzFUTpeyqf0vcvjDA";
 // const navStyle = {
 // 	position: 'absolute',
 // 	top: 0,
@@ -14,67 +14,51 @@ const TOKEN = 'pk.eyJ1IjoidXJjaHlrbGkiLCJhIjoiY2p0Mzk3d2c2MGtnNDN5bHNqaDByOTh0cy
 // 	padding: '10px'
 // };
 class Map extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			viewport: {
-				latitude: 31.954862,
-				longitude: -70.365569,
-				zoom: 3,
-				bearing: 0,
-				pitch: 0,
-				width: 900,
-				height: 500,
-			},
-			marker: {
-				latitude: null,
-				longitude: null,
-			},
-			breweries: []
-		};
-	}
-	// componentDidMount() {
-	// let breweries = this.state.breweries.map(brewery => {
-	// 	return (
-	// 		<div className="brewery-container" key={brewery._id}>
-	// 		  <Link to={brewery._id}>
-	// 			<h1>{brewery.name}</h1>
-	// 			<h3>Location: {park.location}</h3>
-	// 		  </Link>
-	// 		  <button onClick={this.upHandler} name={park._id}>Upvote</button>
-	// 		  <button onClick={this.downHandler} name={park._id}>Downvote</button>
-	// 		</div>
-	// 	)
-	//   })
-	// }
-	render() {
-		const {viewport} = this.state;
-		let {breweries} = this.state.breweries.map(brewery => {
-			<div className="brewery-markers" key={brewery._id}>
-			<Marker latitude={brewery.latitude} longitude={brewery.longitude} offsetLeft={-20} offsetTop={-10}>
-			<Link to={brewery._id}>
-			<img src={icon} width="15" height="50"/>
-			</Link>
-        	</Marker>
-			</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 31.954862,
+        longitude: -70.365569,
+        zoom: 3,
+        bearing: 0,
+        pitch: 0,
+        width: 900,
+        height: 500
+      },
+    //   marker: {
+    //     latitude: null,
+    //     longitude: null
+	//   },
+	  breweries: []
+    };
+  }
 
-		})
-	return (
-		<ReactMapGL {...viewport} mapboxApiAccessToken={TOKEN} mapStyle="mapbox://styles/urchykli/cjt4cqf1x1kpt1frx7xj7g3yq" onViewportChange={(viewport) => { const {width, height, latitude, longitude, zoom} = viewport;
-	// Optionally call `setState` and use the state to update the map.
-}}>
-{...breweries}
+  render() {
+    const { viewport } = this.state;
+
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/urchykli/cjt4cqf1x1kpt1frx7xj7g3yq"
+        onViewportChange={viewport => {
+          const { width, height, latitude, longitude, zoom } = viewport;
+          // Optionally call `setState` and use the state to update the map.
+        }} 
+      >
+        {/* {...breweries} */}
       </ReactMapGL>
-		// <MapGL
-		// 	{...viewport}
-		// 	mapStyle="mapbox://styles/urchykli/cjt4cqf1x1kpt1frx7xj7g3yq"
-		// 	mapboxApiAccessToken={TOKEN}>
-		// 	<div className="nav" style={navStyle}>
-		// 	{/* <NavigationControl/> */}
-		// 	</div>
-		// </MapGL>
-		);
-	}
+      // <MapGL
+      // 	{...viewport}
+      // 	mapStyle="mapbox://styles/urchykli/cjt4cqf1x1kpt1frx7xj7g3yq"
+      // 	mapboxApiAccessToken={TOKEN}>
+      // 	<div className="nav" style={navStyle}>
+      // 	{/* <NavigationControl/> */}
+      // 	</div>
+      // </MapGL>
+    );
+  }
 }
 
 export default Map;
