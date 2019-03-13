e9da2830250acfda8cc7cfbda71be2295b459911
Lindsay Urchyk <urchykli@gmail.com>
2019-03-13 17:17:41 -0400

    beginning changes

--- p4_frontend/src/App.js
+++ p4_frontend/src/App.js
@@ -7,6 +7,68 @@ import Show from './Show'
 
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        city: null,
+        state: null,
+        breweries: [],
+        marker: {
+            latitude: null,
+            longitude: null
+        },
+        realBrewery: [],
+        name: false,
+        brewery: null
+    };
+    this.setBrewery = this.setBrewery.bind(this)
+}
+
+handleCityInput(e) {
+    this.setState({
+        city: e.target.value
+    });
+}
+
+handleStateInput(e) {
+    this.setState({
+        state: e.target.value
+    });
+}
+handleSearchSubmit(e) {
+    e.preventDefault();
+    const url = `https://api.openbrewerydb.org/breweries?by_city=${
+        this.state.city
+    }&by_state=${this.state.state}`;
+    console.log(url);
+    axios
+        .get(url)
+        .then(res => {
+            this.setState({
+                breweries: res.data
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+		this.state.realBrewery = []
+		if(!this.state.name) {
+		for (let i = 0; i < this.state.breweries.length; i++) {
+			if (this.state.breweries[i].longitude !== null) {
+				this.setState({
+					realBrewery: realBrewery.push(this.state.breweries[i]
+				})
+					this.state.realBrewery.push(this.state.breweries[i])
+					this.state.name = true
+			}
+			console.log(this.state.realBrewery);
+		}
+		}
+}
+setBrewery(brewery) {
+  this.setState({brewery: brewery})
+}
 
   render() {
     return (
@@ -19,6 +81,8 @@ class App extends Component {
           <Route path="/" component={Home}/>
           <Route path="/:id" component={Show} />
         </main>
+          <Route path="/" render={(routerProps) => <Home {...routerProps} {...this.state}/>} />
+          <Route path="/:id" render={(routerProps) => <Show setBrewery={this.setBrewery} {...routerProps} {...this.state} />} />
       </div>
     )
   }
